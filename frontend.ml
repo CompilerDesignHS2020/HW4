feffe1854d32d6ce9ebabb83840026060df48ccc
Joël Zolliker <joelzo@stuent.ethz.ch>
2020-11-12 14:32:23 +0100

    wip

--- frontend.ml
+++ frontend.ml
@@ -366,7 +366,21 @@ let cmp_function_ctxt (c:Ctxt.t) (p:Ast.prog) : Ctxt.t =
    in well-formed programs. (The constructors starting with C). 
 *)
 let cmp_global_ctxt (c:Ctxt.t) (p:Ast.prog) : Ctxt.t =
-  failwith "cmp_global_ctxt unimplemented"
+  let rec rec_gctxt rem_prog =
+    begin match rem_prog with
+      | h::tl -> c.Add (begin match h with
+          | gfdecl -> 
+          | gvdecl -> 
+          
+        end)
+      | [] -> []
+    end
+  in
+
+  rec_gctxt c 
+
+
+
 
 (* Compile a function declaration in global context c. Return the LLVMlite cfg
    and a list of global declarations containing the string literals appearing
